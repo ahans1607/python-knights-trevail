2f4e66241ebb0f4e015ceb0d7f1213741f44878d
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-06-03 15:03:24 -0500

    2

---
+++ tree.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value):
+        self._value = value
+        self._parent = None
+        self._children = []
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def children(self):
+        return self._children
+
+    def add_child(self, node):
+        if node not in self._children:
+            self._children.append(node)
+
+    
+    
