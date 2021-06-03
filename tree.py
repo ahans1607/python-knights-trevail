b7273f6658daad8206ae0330779ab7abd1d3f56d
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-06-03 16:00:54 -0500

    3

--- tree.py
+++ tree.py
@@ -12,9 +12,32 @@ class Node:
     def children(self):
         return self._children
 
-    def add_child(self, node):
-        if node not in self._children:
-            self._children.append(node)
+    def add_child(self, newNode):
+        if newNode not in self._children:
+            self._children.append(newNode)
+            newNode.parent = self
+        return
 
-    
+    def remove_child(self, newNode):
+        if newNode in self._children:
+            self._children.remove(newNode)
+            newNode.parent = None
+        return
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, newNode):
+        # if self._parent is not newNode:
+            # if self._parent:
+            #     self._parent.remove_child(newNode)
+
+        self._parent = newNode
+        if newNode is not None:
+            newNode.add_child(self)
+        return
+
+
     
